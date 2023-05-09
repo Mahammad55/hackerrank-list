9ea4a68186051c72151dad2cb2e291f58254f545
Mehemmed <ilyazovmehemmed@gmail.com>
2023-05-09 19:46:15 +0400

    added getlist method

---
+++ src/Arraylist.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Arraylist {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+    }
+
+    public static ArrayList<Integer> getArrayList(Scanner sc) {
+        int countOfArr = sc.nextInt();
+        ArrayList<Integer> subArray = new ArrayList<>();
+        for (int i = 0; i < countOfArr; i++) {
+            subArray.add(sc.nextInt());
+        }
+        return subArray;
+    }
+}
